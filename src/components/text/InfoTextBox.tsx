0a0329cdbb6763f72a03b3c8689e6d3417408f01
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2022-02-16 14:54:08 +0000

    Add react native vector icons into app

---
+++ src/components/text/InfoTextBox.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import { ColorPallet, TextTheme } from '../../theme/theme'
+
+export interface TextBoxProps {
+  children: string
+}
+
+const iconSize = 30
+const offset = 10
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: ColorPallet.notification.info,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: ColorPallet.notification.infoBorder,
+    padding: 10,
+  },
+  headerText: {
+    ...TextTheme.normal,
+    color: ColorPallet.notification.infoText,
+    fontWeight: 'bold',
+    flexShrink: 1,
+    alignSelf: 'center',
+  },
+  icon: {
+    marginRight: offset,
+    alignSelf: 'center',
+  },
+})
+
+const InfoTextBox: React.FC<TextBoxProps> = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <Icon
+          name="info"
+          size={iconSize}
+          color={ColorPallet.notification.infoIcon}
+        />
+      </View>
+      <Text style={styles.headerText}>{children}</Text>
+    </View>
+  )
+}
+
+export default InfoTextBox
