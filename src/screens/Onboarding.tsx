facc853ac49ed166f6a70bbb1160c2ef5b9317a4
dhanashree.tambe <dhanashree.tambe@ayanworks.com>
2022-03-02 16:47:14 +0530

    Resolve conflicts

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -5,26 +5,49 @@ import { useNavigation } from '@react-navigation/native'
 import React, { useState } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import AppIntroSlider from 'react-native-app-intro-slider'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { TextTheme } from '../theme/theme'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.background,
+    margin: 20,
+  },
+  bodyText: {
+    ...TextTheme.normal,
+    flexShrink: 1,
+  },
+  controlsWrapper: {
+    marginTop: 15,
+  },
+  verticalSpacer: {
+    marginVertical: 20,
+  },
+  topSpacer: {
+    paddingTop: 10,
+  },
+})
 
 const slides = [
   {
     key: 1,
     title: 'Title 1',
-    text: 'Description.\nSay something cool',
+    text: 'Description.\nLorem ipsum',
     image: require('../assets/1.png'),
     backgroundColor: '#59b2ab',
   },
   {
     key: 2,
     title: 'Title 2',
-    text: 'Other cool stuff',
+    text: 'Description 2 ./n Lorem ipsum',
     image: require('../assets/2.png'),
     backgroundColor: '#febe29',
   },
   {
     key: 3,
-    title: 'Rocket guy',
-    text: "I'm already out of descriptions\n\nLorem ipsum bla bla bla",
+    title: 'Title 3',
+    text: "I'm already out of descriptions\n\nLorem ipsum ",
     image: require('../assets/3.png'),
     backgroundColor: '#22bcb5',
   },
@@ -35,11 +58,12 @@ const Onboarding: React.FC = () => {
   const navigation = useNavigation()
   const _renderItem = ({ item }) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.bodyText}>{item.title}</Text>
         {/* <Image source={item.image} /> */}
-        <Text>{item.text}</Text>
-      </View>
+        <Text style={styles.bodyText}>{item.text}</Text>
+        <Text style={styles.bodyText}>{item.text}</Text>
+      </SafeAreaView>
     )
   }
   const _onDone = () => {
