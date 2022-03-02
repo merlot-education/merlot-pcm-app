56f125eed739fbc6f524bfeb606eeb3cf902031d
dhanashree.tambe <dhanashree.tambe@ayanworks.com>
2022-03-02 10:45:59 +0530

    refactored email changes

--- App.tsx
+++ App.tsx
@@ -40,50 +40,6 @@ const App = () => {
     encodedVal = md5('abc@gmail.com')
   }
 
-  const handleEmail = () => {
-    Mailer.mail(
-      {
-        subject: 'need help',
-        recipients: ['dhanashree.tambe@ayanworks.com'],
-        ccRecipients: ['supportCC@example.com'],
-        bccRecipients: ['supportBCC@example.com'],
-        body: encodedVal,
-        customChooserTitle: 'This is my new title', // Android only (defaults to "Send Mail")
-        isHTML: true,
-        attachments: [
-          {
-            // Specify either `path` or `uri` to indicate where to find the file data.
-            // The API used to create or locate the file will usually indicate which it returns.
-            // An absolute path will look like: /cacheDir/photos/some image.jpg
-            // A URI starts with a protocol and looks like: content://appname/cacheDir/photos/some%20image.jpg
-            path: '', // The absolute path of the file from which to read data.
-            uri: '', // The uri of the file from which to read the data.
-            // Specify either `type` or `mimeType` to indicate the type of data.
-            type: '', // Mime Type: jpg, png, doc, ppt, html, pdf, csv
-            mimeType: '', // - use only if you want to use custom type
-            name: '', // Optional: Custom filename for attachment
-          },
-        ],
-      },
-      (error, event) => {
-        Alert.alert(
-          error,
-          event,
-          [
-            {
-              text: 'Ok',
-              onPress: () => console.log('OK: Email Error Response'),
-            },
-            {
-              text: 'Cancel',
-              onPress: () => console.log('CANCEL: Email Error Response'),
-            },
-          ],
-          { cancelable: true },
-        )
-      },
-    )
-  }
   const initAgent = async () => {
     convertMD5()
     const newAgent = new Agent(
@@ -115,7 +71,6 @@ const App = () => {
     // RN version can be displayed.
     SplashScreen.hide()
     initAgent()
-    handleEmail()
   }, [])
 
   return (
