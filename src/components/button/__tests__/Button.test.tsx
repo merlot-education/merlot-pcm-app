f9559b5b1497fd9b38a970868a8f84d97fd9b255
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2022-05-02 17:42:37 +0530

    refactor: update button tests

--- src/components/button/__tests__/Button.test.tsx
+++ src/components/button/__tests__/Button.test.tsx
@@ -4,40 +4,25 @@ import Button, { ButtonType } from '../Button'
 
 describe('Button Component', () => {
   it('should render primary button correctly', () => {
-    const testIdName = 'button'
-
-    const { getByTestId, container, getByLabelText } = render(
+    const { container, getByLabelText } = render(
       <Button title="PrimaryButton" buttonType={ButtonType.Primary} />,
     )
-
-    const foundButton = getByTestId(testIdName)
-    expect(foundButton).toBeTruthy()
     expect(container).toMatchSnapshot()
     expect(getByLabelText('PrimaryButton')).toBeTruthy()
   })
 
   it('should render warning button correctly', () => {
-    const testIdName = 'button'
-
-    const { getByTestId, container, getByLabelText } = render(
+    const { container, getByLabelText } = render(
       <Button title="WarningButton" buttonType={ButtonType.Warning} />,
     )
-
-    const foundButton = getByTestId(testIdName)
-    expect(foundButton).toBeTruthy()
     expect(container).toMatchSnapshot()
     expect(getByLabelText('WarningButton')).toBeTruthy()
   })
 
   it('should render ghost button correctly', () => {
-    const testIdName = 'button'
-
-    const { getByTestId, container, getByLabelText } = render(
+    const { container, getByLabelText } = render(
       <Button title="GhostButton" buttonType={ButtonType.Ghost} />,
     )
-
-    const foundButton = getByTestId(testIdName)
-    expect(foundButton).toBeTruthy()
     expect(container).toMatchSnapshot()
     expect(getByLabelText('GhostButton')).toBeTruthy()
   })
