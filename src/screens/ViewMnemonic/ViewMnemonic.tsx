4201d5708c6e5926bab0f5cf845355ca07a7909e
dhanashree.tambe <dhanashree.tambe@ayanworks.com>
2022-05-03 16:15:05 +0530

    view mnemonic test cases

--- src/screens/ViewMnemonic/ViewMnemonic.tsx
+++ src/screens/ViewMnemonic/ViewMnemonic.tsx
@@ -12,12 +12,20 @@ import {
 import ReactNativeBiometrics from 'react-native-biometrics'
 import Toast from 'react-native-toast-message'
 import Clipboard from '@react-native-clipboard/clipboard'
-import { Loader, TextInput } from '../components'
-import Button, { ButtonType } from '../components/button/Button'
-import { ToastType } from '../components/toast/BaseToast'
-import { KeychainStorageKeys } from '../constants'
-import { ColorPallet, TextTheme } from '../theme/theme'
-import { getValueKeychain } from '../utils/keychain'
+import { Loader, TextInput } from '../../components'
+import Button, { ButtonType } from '../../components/button/Button'
+import { ToastType } from '../../components/toast/BaseToast'
+import { KeychainStorageKeys } from '../../constants'
+import { ColorPallet, TextTheme } from '../../theme/theme'
+import { getValueKeychain } from '../../utils/keychain'
+import {
+  authenticateUser,
+  checkIfSensorAvailable,
+  getMnemonicFromKeychain,
+  getValueFromKeychain,
+  showBiometricPrompt,
+} from './ViewMnemonic.utils'
+import { warningToast } from '../../utils/toast'
 
 const style = StyleSheet.create({
   container: {
@@ -59,91 +67,54 @@ const style = StyleSheet.create({
 
 const ViewMnemonic: React.FC = () => {
   const [pin, setPin] = useState('')
-  const [loginAttemtsFailed, setLoginAttemtsFailed] = useState(0)
-  const [biometricFailed, setBiometricFailed] = useState(false)
   const [showMnemonicView, setMnemonicView] = useState(false)
   const [mnemonicText, setMnemonic] = useState('')
   const [loading, setLoading] = useState(false)
   const { t } = useTranslation()
 
-  const biometricEnable = useCallback(() => {
-    ReactNativeBiometrics.isSensorAvailable().then(resultObject => {
-      const { available, biometryType } = resultObject
-      if (available && biometryType === ReactNativeBiometrics.Biometrics) {
-        ReactNativeBiometrics.simplePrompt({
-          promptMessage: t('Biometric.BiometricConfirm'),
-        })
-          .then(async resultObject => {
-            const { success } = resultObject
-            if (success) {
-              showMnemonic()
-
-              //   setAuthenticated(true)
-            } else {
-              Toast.show({
-                type: ToastType.Warn,
-                text1: t('Toasts.Warning'),
-                text2: t('Biometric.BiometricCancle'),
-              })
-              setBiometricFailed(true)
-            }
-          })
-          .catch(() => {
-            Toast.show({
-              type: ToastType.Error,
-              text1: t('Toasts.Warning'),
-              text2: t('Biometric.BiometricFailed'),
-            })
-            setBiometricFailed(true)
-          })
+  const checkBiometricIfPresent = useCallback(async () => {
+    const { available, biometryType } = await checkIfSensorAvailable()
+    if (available && biometryType === ReactNativeBiometrics.Biometrics) {
+      const { success, error } = await showBiometricPrompt()
+      if (success) {
+        setLoading(true)
+        showMnemonic()
+        setLoading(false)
+      } else if (error) {
+        warningToast(error)
       } else {
-        Toast.show({
-          type: ToastType.Warn,
-          text1: t('Toasts.Warning'),
-          text2: t('Biometric.BiometricNotSupport'),
-        })
+        warningToast(t('Biometric.BiometricCancle'))
       }
-    })
+    } else {
+      warningToast(t('Biometric.BiometricNotSupport'))
+    }
   }, [t])
 
   useEffect(() => {
-    ReactNativeBiometrics.isSensorAvailable().then(resultObject => {
-      const { available, biometryType } = resultObject
-      if (
-        available &&
-        biometryType === ReactNativeBiometrics.Biometrics &&
-        !biometricFailed
-      ) {
-        biometricEnable()
-      }
-    })
-  }, [biometricEnable, biometricFailed])
+    checkBiometricIfPresent()
+  }, [checkBiometricIfPresent])
 
   const checkPin = async (pin: string) => {
-    const keychainEntry = await getValueKeychain({
-      service: 'passcode',
-    })
-    if (keychainEntry && pin === keychainEntry.password) {
+    const [passcode] = await Promise.all([
+      new Promise(resolve => {
+        resolve(getValueFromKeychain(KeychainStorageKeys.Passcode))
+      }),
+    ])
+    const params = [pin, passcode.password]
+    const result = authenticateUser(params)
+    if (result) {
       showMnemonic()
     } else {
-      Toast.show({
-        type: ToastType.Warn,
-        text1: t('Toasts.Warning'),
-        text2: t('PinEnter.IncorrectPin'),
-      })
-      setLoginAttemtsFailed(loginAttemtsFailed + 1)
-      if (loginAttemtsFailed === 5) {
-        Alert.alert(t('Registration.RegisterAgain'))
-        // navigation.navigate(Screens.Registration, { forgotPin: false })
-        // await AsyncStorage.removeItem(LocalStorageKeys.OnboardingCompleteStage)
-      }
+      warningToast(t('PinEnter.IncorrectPin'))
     }
   }
 
   const showMnemonic = async () => {
-    const passphraseEntry = await getValueKeychain({
-      service: KeychainStorageKeys.Passphrase,
-    })
+    const [passphraseEntry] = await Promise.all([
+      new Promise(resolve => {
+        resolve(getMnemonicFromKeychain())
+      }),
+    ])
     setMnemonic(passphraseEntry.password)
     setMnemonicView(true)
   }
---
+++ src/screens/ViewMnemonic/ViewMnemonic.utils.test.ts
@@ -0,0 +1,13 @@
+import * as Utils from './ViewMnemonic.utils'
+
+describe('ViewMnemonic.utils', () => {
+  describe('authenticateuser', () => {
+    it.each([
+      { args: [123456, 123456], expected: true },
+      { args: [111111, 111222], expected: false },
+    ])('should return a true if the pin matches', initialValue => {
+      const result = Utils.authenticateUser(initialValue.args)
+      expect(result).toEqual(initialValue.expected)
+    })
+  })
+})
