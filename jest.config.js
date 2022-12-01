8250aa3158f190f09e46915f1b4207b79bf3b33a
Alexey Lunin <alexey.lunin@vereign.com>
2022-12-01 12:35:14 +0300

    push package json changes

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,4 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-argon2|react-native-toast-message||react-native-camera|react-native-dropdown-picker)/)',
   ],
-}
+};
