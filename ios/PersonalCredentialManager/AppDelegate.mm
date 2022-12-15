32c171d4a2e9594da226e1d8587624bdf366c709
Alexey Lunin <alexey.lunin@vereign.com>
2022-12-15 05:46:15 +0300

    fix deep link implementation on ios device

--- ios/PersonalCredentialManager/AppDelegate.mm
+++ ios/PersonalCredentialManager/AppDelegate.mm
@@ -5,6 +5,7 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
+#import <React/RCTLinkingManager.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -129,5 +130,13 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 }
 
 #endif
+
+// Add this inside `@implementation AppDelegate` above `@end`:
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 
 @end
