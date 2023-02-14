7d7a890dd7cdf22bbbaa11a0b5018b0a6bdc6c63
Alexey Lunin <alexey.lunin@vereign.com>
2023-02-14 06:39:19 +0300

    Change order of ledgers

--- configs/ledgers/indy/index.ts
+++ configs/ledgers/indy/index.ts
@@ -3,4 +3,4 @@ import IDunionTest from './idunion-test/pool-config';
 import IDunionBosch from './idunion-bosch/pool-config';
 import GreenLightBcovrin from './green-light-bcovrin/pool-config';
 
-export default [BCovrinTest, IDunionBosch, IDunionTest, GreenLightBcovrin];
+export default [IDunionTest, IDunionBosch, BCovrinTest, GreenLightBcovrin];
